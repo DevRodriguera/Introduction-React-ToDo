08a0807fe2dafc197577c85b91fcb94b44314ba8
DevRodriguera <souzarodrigo7@hotmail.com>
2023-10-06 14:25:02 -0300

    first commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const hadleSubmit = (e) => {
+        e.preventDefault();
+        if(!value || !category) return;
+        addTodo(value,category);
+        setValue("");
+        setCategory("");
+    };
+
+    return <div className="todo-form">
+    <h2>Criar tarefa</h2>
+    <form onSubmit={hadleSubmit}>
+        <input type="text" id="" placeholder="Digite o título" value={value} onChange={(e) => setValue(e.target.value)}/>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+            <option value="">Selecione uma opção</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Pessoal">Pessoal</option>
+            <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar tarefa</button>
+    </form>
+    
+  </div>
+}
+
+export default TodoForm
